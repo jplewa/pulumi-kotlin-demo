a4efdded29021f50943fc42814834c3c461b12c9
Julia Plewa <jplewa@virtuslab.com>
2023-08-31 17:01:41 +0200

    Update dependencies

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.pulumi:pulumi:(,1.0]")
+    implementation("org.virtuslab:pulumi-kotlin:0.9.4.0")
     implementation("org.virtuslab:pulumi-kubernetes-kotlin:4.1.1.0")
     implementation("org.virtuslab:pulumi-gcp-kotlin:6.62.0.0")
     implementation("org.virtuslab:pulumi-docker-kotlin:4.3.0.0")
