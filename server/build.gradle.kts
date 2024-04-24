7ef79a23c872dc7a45fb95ff033b663666f6fbbd
Julia Plewa <jplewa@virtuslab.com>
2024-04-24 18:26:01 +0200

    Update dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -9,11 +9,11 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.3")
-    implementation("io.ktor:ktor-server-netty:2.3.3")
+    implementation("io.ktor:ktor-server-core:2.3.10")
+    implementation("io.ktor:ktor-server-netty:2.3.10")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.3")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 application {
